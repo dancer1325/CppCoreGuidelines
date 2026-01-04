30842d83fdd958ccbf89421a2f55594c33704ef8
dancer1325 <alfredotic0809@gmail.com>
2026-01-04 13:22:46 +0100

    docs: clear notes

---
+++ examples/NL/5/good/foo.cpp
@@ -0,0 +1,5 @@
+void print(int i);
+void print(string_view);    // also works on any string-like sequence
+
+print(1);              // clear, automatic type matching
+print("xyzzy");        // clear, automatic type matching
